4fb3d1c5db7d95479bbf11bcbdb66045714e5cd3
BilelMagherby <lartisto44@gmail.com>
2025-12-15 18:30:54 +0100

    translate excursion

--- src/Form/ReservationVoitureType.php
+++ src/Form/ReservationVoitureType.php
@@ -16,44 +16,44 @@ class ReservationVoitureType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class, [
-                'label' => 'Nom',
+                'label' => 'form.voiture.nom',
                 'attr' => ['required' => true]
             ])
-            ->add('prenom', TextType::class, ['label' => 'Prénom'])
+            ->add('prenom', TextType::class, ['label' => 'form.voiture.prenom'])
             ->add('email', \Symfony\Component\Form\Extension\Core\Type\EmailType::class, [
-                'label' => 'Email',
-                'attr' => ['placeholder' => 'exemple@email.com']
+                'label' => 'form.voiture.email',
+                'attr' => ['placeholder' => 'form.voiture.placeholder_email']
             ])
             ->add('dateNaissance', DateType::class, [
-                'label' => 'Date de naissance',
+                'label' => 'form.voiture.date_naissance',
                 'widget' => 'single_text',
                 'attr' => [
                     'required' => true,
                     'max' => (new \DateTime('-18 years'))->format('Y-m-d')
                 ]
             ])
-            ->add('lieuNaissance', TextType::class, ['label' => 'Lieu de naissance'])
-            ->add('nationalite', TextType::class, ['label' => 'Nationalité'])
-            ->add('adresse', TextType::class, ['label' => 'Adresse'])
-            ->add('tel', TelType::class, ['label' => 'Téléphone'])
-            ->add('numCinPassport', TextType::class, ['label' => 'CIN / Passeport'])
-            ->add('cinDelivreLe', DateType::class, [ 'label' => 'CIN délivré le', 'widget' => 'single_text', 'required' => true ])
-            ->add('numPermis', TextType::class, ['label' => 'Numéro de permis', 'required' => true])
+            ->add('lieuNaissance', TextType::class, ['label' => 'form.voiture.lieu_naissance'])
+            ->add('nationalite', TextType::class, ['label' => 'form.voiture.nationalite'])
+            ->add('adresse', TextType::class, ['label' => 'form.voiture.adresse'])
+            ->add('tel', TelType::class, ['label' => 'form.voiture.tel'])
+            ->add('numCinPassport', TextType::class, ['label' => 'form.voiture.cin'])
+            ->add('cinDelivreLe', DateType::class, [ 'label' => 'form.voiture.cin_date', 'widget' => 'single_text', 'required' => true ])
+            ->add('numPermis', TextType::class, ['label' => 'form.voiture.permis_num', 'required' => true])
             ->add('permisDelivreLe', DateType::class, [
-                'label' => 'Permis délivré le',
+                'label' => 'form.voiture.permis_date',
                 'widget' => 'single_text',
                 'attr' => [
                     'max' => (new \DateTime('-2 years'))->format('Y-m-d')
                 ]
             ])
-            ->add('permisLieuDelivrance', TextType::class, ['label' => 'Lieu de délivrance du permis', 'required' => true])
+            ->add('permisLieuDelivrance', TextType::class, ['label' => 'form.voiture.permis_lieu', 'required' => true])
             ->add('dateDebut', DateType::class, [
-                'label' => 'Date de début de location',
+                'label' => 'form.voiture.date_debut',
                 'widget' => 'single_text',
                 'attr' => ['min' => (new \DateTime())->format('Y-m-d')]
             ])
             ->add('dateFin', DateType::class, [
-                'label' => 'Date de fin de location',
+                'label' => 'form.voiture.date_fin',
                 'widget' => 'single_text',
                 'attr' => ['min' => (new \DateTime())->format('Y-m-d')]
             ]);
